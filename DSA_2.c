3977b39a54585ef755ded3051d57f3384a5457a4
Swastik-Savanur <01fe24bec306@kletech.ac.in>
2026-04-15 09:36:16 +0530

    DSA_3.c

--- DSA_2.c
+++ DSA_2.c
@@ -1,73 +1,55 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
 {
-    int data;
+    int id;
     struct node *left;
     struct node *right;
 }*node;
 node insert(node root);
-node delete_node(node root,int data);
-void preorder(node root);
+node delete_(node root,int id);
 void inorder(node root);
-void postorder(node root);
 int main()
 {
-   node root=NULL;
-   int choice,data;
-   while(1)
-   {
-       printf("\n enter the choice :");
-       scanf("%d",&choice);
-       switch(choice)
-       {
-           case 0:exit(0);break;
-           case 1:root=insert(root);
-           break;
-           case 2:
-               printf("enter data :\n");
-               scanf("%d",&data);
-               root=delete_node(root,data);
-           break;
-           case 3:preorder(root);
-           break;
-           case 4:inorder(root);
-           break;
-           case 5:postorder(root);
-           break;
-           default:
-            printf("invalid choice:\n");
-            break;
-       }
-   }
-   return 0;
+    node root=NULL;
+    int n,data;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        root=insert(root);
+    }
+    root = insert(root);
+    scanf("%d",&data);
+    root=delete_(root,data);
+    printf("Current File Index sorted in ascending order: ");
+    inorder(root);
+    return 0;
 }
 node create()
 {
     node newnode=(node)malloc(sizeof(struct node));
     if(newnode==NULL)
     {
-        printf("the memory is not allocated:\n");
         return NULL;
     }
-    printf("enter the data :\n");
-    scanf("%d",&newnode->data);
+    scanf("%d",&newnode->id);
     newnode->left=newnode->right=NULL;
     return newnode;
 }
 node insert(node root)
 {
     node newnode=create();
-    node cur=root,parent=NULL;
     if(root==NULL)
     {
         root=newnode;
     }
     else{
+        node cur=root,parent=NULL;
         while(cur!=NULL)
         {
             parent=cur;
-            if(newnode->data<cur->data)
+            if(newnode->id<cur->id)
             {
                 cur=cur->left;
             }
@@ -75,7 +57,7 @@ node insert(node root)
                 cur=cur->right;
             }
         }
-        if(newnode->data<parent->data)
+        if(newnode->id<parent->id)
         {
             parent->left=newnode;
         }
@@ -85,95 +67,72 @@ node insert(node root)
     }
     return root;
 }
-void preorder(node root)
-{
-    if(root!=NULL)
-    {
-        printf("%d ",root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
-void inorder(node root)
-{
-    if(root!=NULL)
-    {
-        inorder(root->left);
-        printf("%d ",root->data);
-        inorder(root->right);
-    }
-}
-void postorder(node root)
-{
-    if(root!=NULL)
-    {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ",root->data);
-    }
-}
-node delete_node(node root,int data)
+node delete_(node root,int id)
 {
     if(root==NULL)
     {
-        printf("tree is empty :\n");
-        return root;
+       return root;
     }
     else{
         node cur=root,parent=NULL;
-        while(cur!=NULL&&data!=cur->data)
+        while(cur!=NULL&&id!=cur->id)
         {
-            parent=cur;
-            if(data<cur->data)
-            {
-                cur=cur->left;
-            }
-            else{
-                cur=cur->right;
-            }
-        }
 
+             parent=cur;
+             if(id<cur->id)
+             {
+                 cur=cur->left;
+             }
+             else{
+                cur=cur->right;
+             }
+        }
         if(cur==NULL)
         {
-            printf("the element is not found:\n");
             return root;
         }
-        //node has two children
         if(cur->left!=NULL&&cur->right!=NULL)
         {
-            node sc=cur->right;
-            node sparent=cur;
-        while(sc->left!=NULL)
-        {
-            sparent=sc;
-            sc=sc->left;
+            node c=cur->right;
+            node cp=c;
+            while(c->left!=NULL)
+            {
+                cp=c;
+                c=c->left;
+            }
+            cur->id=c->id;
+            cur=c;
+            parent=cp;
         }
-        cur->data=sc->data;
-        cur=sc;
-        parent=sparent;
-      }
-      //case 2 one child OR No child
-      node child;
-      if(cur->left!=NULL)
-      {
-          child=cur->left;
-      }
-      else{
-        child=cur->right;
-      }
-      if(parent==NULL)
-      {
-          free(cur);
-          return child;
-      }
-      if(parent->left==cur)
-      {
-          parent->left=child;
-      }
-      else{
-        parent->right=child;
-      }
-      free(cur);
+        node child=NULL;
+        if(cur->left!=NULL)
+        {
+            child=cur->left;
+        }
+        else{
+            child=cur->right;
+        }
+        if(parent==NULL)
+        {
+            free(cur);
+            return child;
+        }
+        if(parent->left==cur)
+        {
+            parent->left=child;
+        }
+        else{
+            parent->right=child;
+        }
+        free(cur);
     }
     return root;
+}
+void inorder(node root)
+{
+    if(root!=NULL){
+    inorder(root->left);
+    printf("%d ",root->id);
+    inorder(root->right);
+    }
 }
